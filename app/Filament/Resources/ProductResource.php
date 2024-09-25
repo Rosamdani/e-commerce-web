878117a21632a150ddfc2cbd076e7ab7efcaf4d4
Rosamdani <rosyamdani2000018114@webmail.uad.ac.id>
2024-09-26 00:00:47 +0700

    Edit: Layouting product create

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -8,7 +8,11 @@ use App\Models\Products;
 use Filament\Forms;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Group;
 use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -28,44 +32,79 @@ class ProductResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('nama')
-                    ->required()
-                    ->placeholder('Masukkan nama produk'),
-                Forms\Components\TextInput::make('stok')
-                    ->required()
-                    ->numeric()
-                    ->minValue(1)
-                    ->maxValue(10000)
-                    ->step(1)
-                    ->placeholder('Masukkan angka'),
-                Forms\Components\TextInput::make('harga')
-                    ->required()
-                    ->numeric()
-                    ->placeholder(placeholder: 'Masukkan harga produk'),
-                Grid::make(1) // Menggunakan satu kolom untuk full width
-                    ->schema([
-                        MarkdownEditor::make('deskripsi')
-                            ->toolbarButtons([
-                                'blockquote',
-                                'bold',
-                                'bulletList',
-                                'codeBlock',
-                                'heading',
-                                'italic',
-                                'orderedList',
-                                'redo',
-                                'undo',
-                            ]),
-                        FileUpload::make('product_image')->multiple()
-                            ->image()
-                            ->imageEditor()
-                            ->imageEditorAspectRatios([
-                                '1:1',
-                            ])
-                            ->directory('product_images'),,
-                    ]),
+                Group::make()->schema([
+                    Section::make('Product Information')
+                        ->description('Product Information')
+                        ->schema([
+                            TextInput::make('nama')
+                                ->label('Nama Produk')
+                                ->required()
+                                ->placeholder('Masukkan nama produk'),
+                            TextInput::make('stok')
+                                ->required()
+                                ->numeric()
+                                ->minValue(1)
+                                ->maxValue(10000)
+                                ->step(1)
+                                ->placeholder('Masukkan angka'),
+                            TextInput::make('harga')
+                                ->required()
+                                ->numeric()
+                                ->placeholder(placeholder: 'Masukkan harga produk'),
+                            Grid::make(1) // Menggunakan satu kolom untuk full width
+                                ->schema([
+                                    MarkdownEditor::make('deskripsi')
+                                        ->toolbarButtons([
+                                            'blockquote',
+                                            'bold',
+                                            'bulletList',
+                                            'codeBlock',
+                                            'heading',
+                                            'italic',
+                                            'orderedList',
+                                            'redo',
+                                            'undo',
+                                        ]),
+                                ]),
+                        ])->columns(2),
+                    Section::make("Galeri Produk")
+                        ->description("Galeri Produk")
+                        ->schema([
+                            FileUpload::make('product_image')->multiple()
+                                ->image()
+                                ->required()
+                                ->imageEditor()
+                                ->imageEditorAspectRatios([
+                                    '1:1',
+                                ])
+                                ->minFiles(1)
+                                ->maxFiles(5)
+                                ->directory('product_images'),
+                        ]),
 
-            ]);
+                ])->columnSpan([
+                    'sm' => 3,
+                    'lg' => 2,
+                ]),
+                Group::make()->schema([
+                    Section::make('Visibilitas')->description('Visibilitas Produk')->schema([
+                        Select::make('status')
+                            ->label('Status')
+                            ->options([
+                                'active' => 'Active',
+                                'disabled' => 'Disabled',
+                            ])
+                            ->native(false)
+                    ]),
+                    Section::make('Tags')->schema([
+                        TagsInput::make('tags')
+                            ->label('Tags Produk')
+                    ]),
+                ])->columnSpan([
+                    'sm' => 3,
+                    'lg' => 1,
+                ])
+            ])->columns(3);
     }
 
     public static function table(Table $table): Table
